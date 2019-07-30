1d2f57872917c4099d8b9fc692444b609c6b755b
Paul T <toepfer@aidaorga.de>
2019-07-30 13:24:46 +0200

    Added prints for debug measurements

--- main.py
+++ main.py
@@ -3,19 +3,23 @@ from ftplib import FTP
 from discord.ext import commands
 import os
 
+print("Starting Script")
 
 def writeline(data):
      file.write(data)
      file.write(os.linesep)
 
+print("Connecting to FTP Server")
 conn = FTP(host=os.environ.get('FTPHost'))
 conn.connect()
 conn.login(user=os.environ.get('FTPUser'),passwd=os.environ.get('FTPPass'))
 
+print("Writing data.json from FTP")
 file = open('Cogs/data.json', 'w+')
 conn.retrlines('RETR data.json', writeline)
 file.close()
 
+print("Writing userdata.json from FTP")
 file = open('Cogs/userdata.json', 'w+')
 conn.retrlines('RETR userdata.json', writeline)
 file.close()
@@ -34,10 +38,11 @@ async def on_ready():
         await bot.logout()
 
 
-
+print("Load Cogs")
 # Load the different modules from the Cogs folder
 bot.load_extension("Cogs.UserInteraction")
 bot.load_extension("Cogs.BuddyJam")
 
+print("Start bot")
 # Starts the bot
 bot.run(os.environ.get('token'))
